e76fa0170797c406ba48bda3e2e2dbb7d4d5d05c
Divya Aggarwal <divya@mail.vinove.com>
2018-10-03 11:56:48 +0530

    First Commit

---
+++ DemoPush/DemoPush/DemoPush.Android/Gcm.Client/GcmBroadcastReceiverBase.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//using Android.App;
+//using Android.Content;
+//using Android.OS;
+//using Android.Runtime;
+//using Android.Views;
+//using Android.Widget;
+
+//namespace DemoPush.Droid.Gcm.Client
+//{
+//    class GcmBroadcastReceiverBase
+//    {
+//    }
+//}
+using Android.App;
+using Android.Content;
+
+
+namespace DemoPush.Droid.Gcm.Client
+{
+    public abstract class GcmBroadcastReceiverBase<TIntentService> : BroadcastReceiver where TIntentService : GcmServiceBase
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Logger.Debug("OnReceive: " + intent.Action);
+            var className = context.PackageName + Constants.DEFAULT_INTENT_SERVICE_CLASS_NAME;
+
+            Logger.Debug("GCM IntentService Class: " + className);
+
+            GcmServiceBase.RunIntentInService(context, intent, typeof(TIntentService));
+            SetResult(Result.Ok, null, null);
+        }
+    }
+}
